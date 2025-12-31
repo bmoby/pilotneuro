f010069e065edba590c6b9e2e6702300f07d6f21
tsarag <msouleymanov@gmail.com>
2025-12-31 14:42:02 +0700

    perfectionnement

--- src/components/KerbSeparator.tsx
+++ src/components/KerbSeparator.tsx
@@ -7,15 +7,11 @@ import styles from "./KerbSeparator.module.css";
 export default function KerbSeparator() {
     return (
         <div className={styles.container} role="separator" aria-hidden="true">
-            <div className={`${styles.line} ${styles.lineLeft}`} />
-
             <div className={styles.accent}>
                 <div className={`${styles.dash} ${styles.red}`} />
                 <div className={`${styles.dash} ${styles.yellow}`} />
                 <div className={`${styles.dash} ${styles.red}`} />
             </div>
-
-            <div className={`${styles.line} ${styles.lineRight}`} />
         </div>
     );
 }
